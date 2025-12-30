72d747763c8003271d3ec5325bdab4aac8fb8760
robertoweb <86133094+web-roberto@users.noreply.github.com>
2025-12-30 23:25:47 +0100

    Update tools_list.py

--- tools_list.py
+++ tools_list.py
@@ -12,7 +12,7 @@ st.toast("Loading...")
 links = [
     {"href": "https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py",
     "text": "Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines",
-    "background":#2ecc71 },
+    "background":cyan },
     # 1_1_1_hacking_buscadores_parte1
 ]
 
