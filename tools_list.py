090998d6efad9e7c690059d1607452f6ec10e4f4
robertoweb <86133094+web-roberto@users.noreply.github.com>
2025-12-30 22:53:54 +0100

    Update tools_list.py

--- tools_list.py
+++ tools_list.py
@@ -17,25 +17,14 @@ with col1:
     st.markdown(
     """
     <a href="https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py" target="_blank">
-        <button style="
-            background-color:#2ecc71;
-            color:white;
-            padding:0.5em 1em;
-            margin:0.5em;
-            border:none;
-            border-radius:8px;
-            margin-right: auto;
-            cursor:pointer;
-        ">
+        <button style="background-color:#2ecc71;color:white;padding:0.5em 1em;margin:0.5em;border:none;border-radius:8px;margin-right: auto;cursor:pointer;">
             Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines
         </button>
     </a>
     """,
     unsafe_allow_html=True
 )
-    st.success("Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines") # 1_1_1_hacking_buscadores_parte1
-    st.link_button("Code Github", "https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py")
-    st.write('')
+    # st.success("Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines") # 1_1_1_hacking_buscadores_parte1
     # st.link_button("Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines","https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py")
     st.info("Roberto's Cybersecurity Tool -> Google Hacking -> Execution of Python Scripts")  # 1_1_3_ejecucion_scripts_python -> 40
     st.error("Roberto's Cybersecurity Tool -> Google Hacking -> Command Line Arguments")  # 1_1_4_argumentos_linea_comandos -> 41
@@ -122,8 +111,8 @@ with col1:
     st.error("Roberto's Cybersecurity Tool -> Evasion Defenses")  # 8_6_evasion_defensas
     st.success("Roberto's Cybersecurity Tool -> Exfiltration Steganography")  # 8_7_exfiltracion_esteganografia
 with col2:
-    st.link_button("Code Github", "https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py")
-    st.write('')
+    #st.link_button("Code Github", "https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py")
+    #st.write('')
     st.link_button("Code Github", "https://github.com/web-roberto/cybersecurity_python/tree/main/1_1_3_ejecucion_scripts_python")
     st.write('')
     st.link_button("Code Github", "https://github.com/web-roberto/cybersecurity_python/tree/main/1_1_4_argumentos_linea_comandos")
