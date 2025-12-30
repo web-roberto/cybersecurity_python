1c64b151c4878feea2a89b0042c67a1cdbd7c1bb
robertoweb <86133094+web-roberto@users.noreply.github.com>
2025-12-30 11:25:41 +0100

    Update tools_list.py

--- tools_list.py
+++ tools_list.py
@@ -12,14 +12,14 @@ st.toast("Loading...")
 col1, col2 = st.columns([6, 1], gap="small", vertical_alignment="top", border=True, width="stretch")
 
 with col1:
-    st.success("Ethical hacking Tool: GOOGLE HACKING-> Hacking search engines") //1_1_1_hacking_buscadores_parte1
-    st.success("Ethical hacking Tool: GOOGLE HACKING-> ") //1_1_3_ejecucion_scripts_python -> 40
-    st.success("Ethical hacking Tool: GOOGLE HACKING-> ") //1_1_4_argumentos_linea_comandos -> 41
-    st.success("Ethical hacking Tool: GOOGLE HACKING-> ") //1_1_5_generacion_resultados -> 42
-    st.success("Ethical hacking Tool: GOOGLE HACKING-> ") //1_1_6_manejo_ficheros -> 43
-    st.success("Ethical hacking Tool: GOOGLE HACKING-> ") //1_1_7_dorks_ia_GPT4All -> 47
-    st.success("Ethical hacking Tool: GOOGLE HACKING with AI-> ") //1_1_8_dorks_ia_OpenAI_GPT4_Parte1 -> 47
-    st.success("Ethical hacking Tool: GOOGLE HACKING with AI-> ") //1_1_9_dorks_ia_OpenAI_GPT4_Parte2 ->48
+    st.success("Ethical hacking Tool: Google Hacking -> Hacking search engines") //1_1_1_hacking_buscadores_parte1
+    st.success("Ethical hacking Tool: Google Hacking -> ") //1_1_3_ejecucion_scripts_python -> 40
+    st.success("Ethical hacking Tool: Google Hacking -> ") //1_1_4_argumentos_linea_comandos -> 41
+    st.success("Ethical hacking Tool: Google Hacking -> ") //1_1_5_generacion_resultados -> 42
+    st.success("Ethical hacking Tool: Google Hacking -> ") //1_1_6_manejo_ficheros -> 43
+    st.success("Ethical hacking Tool: Google Hacking -> ") //1_1_7_dorks_ia_GPT4All -> 47
+    st.success("Ethical hacking Tool: Google Hacking with AI-> ") //1_1_8_dorks_ia_OpenAI_GPT4_Parte1 -> 47
+    st.success("Ethical hacking Tool: Google Hacking with AI-> ") //1_1_9_dorks_ia_OpenAI_GPT4_Parte2 ->48
     st.success("Ethical hacking Tool: Information filtering with RegEx -> ") //1_2_1_filtrado_informacion_regex -> smartsearch.pyv- video 49
     st.success("Ethical hacking Tool: Information filtering with AI-> ") //1_2_2_filtrado_informacion_IA->52
     st.success("Ethical hacking Tool: NinjaDork and Selenium -> ") //1_3_automatizacion_selenium -> 52
