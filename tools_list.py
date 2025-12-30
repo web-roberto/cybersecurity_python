1e922bd2bfbaf5eff19f59618e0ecc2a8bdf1a6c
robertoweb <86133094+web-roberto@users.noreply.github.com>
2025-12-30 23:10:24 +0100

    Update tools_list.py

--- tools_list.py
+++ tools_list.py
@@ -16,12 +16,11 @@ col1, col2 = st.columns([6, 1], gap="small", vertical_alignment="top", border=Tr
 with col1:
     # st.success("Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines") # 1_1_1_hacking_buscadores_parte1
     st.markdown(
-    """<p>
-    <a href="https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py" target="_blank">
+    """<a href="https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py" target="_blank">
         <button style="background-color:#2ecc71;color:white;padding:0.5em 1em;margin:0.5em;width:100%;border:none;text-align: start;border-radius:8px;cursor:pointer;">
             Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines
         </button>
-    </a></p>""",unsafe_allow_html=True)
+    </a>""",unsafe_allow_html=True)
     st.info("Roberto's Cybersecurity Tool -> Google Hacking -> Execution of Python Scripts")  # 1_1_3_ejecucion_scripts_python -> 40
     st.error("Roberto's Cybersecurity Tool -> Google Hacking -> Command Line Arguments")  # 1_1_4_argumentos_linea_comandos -> 41
     st.warning("Roberto's Cybersecurity Tool -> Google Hacking -> Generation of Results")  # 1_1_5_generacion_resultados -> 42
