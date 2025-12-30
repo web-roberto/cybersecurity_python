719943e6302e48e47a3aa2bbd89bcae5c9e1cf9c
robertoweb <86133094+web-roberto@users.noreply.github.com>
2025-12-30 22:41:42 +0100

    Update tools_list.py

--- tools_list.py
+++ tools_list.py
@@ -8,12 +8,30 @@ st.write("80 Cyber Tools by Roberto")
 st.balloons()
 st.snow()
 st.toast("Loading...")
+st.markdown(
+    """
+    <a href="https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py" target="_blank">
+        <button style="
+            background-color:#2ecc71;
+            color:white;
+            padding:0.5em 1em;
+            border:none;
+            border-radius:8px;
+            cursor:pointer;
+        ">
+            Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines
+        </button>
+    </a>
+    """,
+    unsafe_allow_html=True
+)
+
 
 col1, col2 = st.columns([6, 1], gap="small", vertical_alignment="top", border=True, width="stretch")
 
 with col1:
     st.success("Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines") # 1_1_1_hacking_buscadores_parte1
-    st.link_button("Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines","https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py",help=None, type="secondary", icon=None, disabled=False, use_container_width=None, width="content", shortcut=None)
+    st.link_button("Roberto's Cybersecurity Tool -> Google Hacking -> Hacking search engines","https://github.com/web-roberto/cybersecurity_python/blob/main/1_1_1_hacking_buscadores_parte1/ninjadorks.py")
     st.info("Roberto's Cybersecurity Tool -> Google Hacking -> Execution of Python Scripts")  # 1_1_3_ejecucion_scripts_python -> 40
     st.error("Roberto's Cybersecurity Tool -> Google Hacking -> Command Line Arguments")  # 1_1_4_argumentos_linea_comandos -> 41
     st.warning("Roberto's Cybersecurity Tool -> Google Hacking -> Generation of Results")  # 1_1_5_generacion_resultados -> 42
